43fc774b44a28ad1ef4ee218ed909c51d6038168
Emrah Yurtlu <emrahyurtlu@gmail.com>
2020-02-15 23:29:21 +0300

    Main java güncellendi.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,7 @@ import com.lavasoft.GeoIPServiceSoap;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-        * WSDL: http://wsgeoip.lavasoft.com/ipservice.asmx?WSDL
-        * wsimport komutu ile yukarıdaki wsdl kullanılarak sınıfları
-        * oluşturduk ve projeye ekledik.
-        *
-        * */
-        
+
         String ipAddress = "94.55.118.33";
 
         GeoIPService geoIPService = new GeoIPService();
